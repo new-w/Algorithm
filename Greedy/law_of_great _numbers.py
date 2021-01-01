c83e32b728d82c7b1e0cf46e00bda9e8e56b8d93
new-w <newww0120@gmail.com>
2021-01-02 01:32:45 +0900

    day2

---
+++ Greedy/law_of_great _numbers.py
@@ -0,0 +1,13 @@
+N, M, K = input("input N, M, K : ").split()
+N = int(N)
+M = int(M)
+K = int(K)
+
+num_list = input("input values : ").split()
+num_list = list(map(int, num_list))
+
+sum = 0
+for j in range(0, M):
+    for i in range(0, K):
+        sum+=max(num_list)
+    M = M - K
