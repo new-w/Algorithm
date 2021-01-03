5143ac03f404b05a39ab97735434f5e98f05c1f9
new-w <newww0120@gmail.com>
2021-01-03 23:57:09 +0900

    day?

--- Greedy/law_of_great _numbers.py
+++ Greedy/law_of_great _numbers.py
@@ -1,18 +1,26 @@
-N, M, K = input("input N, M, K : ").split()
+N, M, K = input("input N, M, K: ").split()
 N = int(N)
 M = int(M)
 K = int(K)
 
-num_list = input("input values : ").split()
+num_list = input("input values  ").split()
 num_list = list(map(int, num_list))
 
 # find maximum value
+max1 = max(num_list)
+num_list.remove(max1)
 
-# find second maximum valueK
+# find second maximum value
+max2 = max(num_list)
 
-
-sum = 0
-for j in range(0, M):
+a = M // (K+1)
+b = M % (K+1)
+sum=0
+for j in range(0, a):
     for i in range(0, K):
-        sum+=max(num_list)
-    M = M - K
+        sum+=max1
+    sum+=max2
+for l in range(0, b):
+    sum+=max1
+
+print("sum: ", sum)
