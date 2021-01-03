bba0d69457d6a620e5acbf9385d968dda9e40c0f
new-w <newww0120@gmail.com>
2021-01-03 16:31:28 +0900

    cont

--- Greedy/law_of_great _numbers.py
+++ Greedy/law_of_great _numbers.py
@@ -6,6 +6,11 @@ K = int(K)
 num_list = input("input values : ").split()
 num_list = list(map(int, num_list))
 
+# find maximum value
+
+# find second maximum valueK
+
+
 sum = 0
 for j in range(0, M):
     for i in range(0, K):
