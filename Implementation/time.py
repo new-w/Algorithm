d375724de2f91c76cb5b8b58415c8017ad1ef359
new-w <newww0120@gmail.com>
2021-01-07 19:28:32 +0900

    low cost

--- Implementation/time.py
+++ Implementation/time.py
@@ -1,21 +1,21 @@
-#N = int(input("input N: "))
 for N in range(0, 24):
-    if N<3:
+    if (N>=0) and (N<3):
         K=0
-        if N==0:
-            K=0
-            N=1
+        C=0
     elif (N>=3) and (N<13):
         K=1
+        C=1575
     elif (N>=13) and (N<23):
         K=2
+        C=3150
     elif (N>=23):
         K=3
+        C=4725
 
     h = K * 60 * 60
-    m = N * 60 * 15
-    s = N * 60 * 15
-    common = N * 15 * 15
+    m = (N+1) * 60 * 15
+    s = (N+1) * 60 * 15
+    common = ((N+1) * 15 * 15) + C
 
-    result = h + m + s - common
+    result = (h + m + s) - common
     print(N, ":", result)
