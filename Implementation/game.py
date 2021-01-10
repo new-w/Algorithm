384f3f448d48455847926cbeef6dd381df3e9d59
new-w <newww0120@gmail.com>
2021-01-10 18:21:34 +0900

    day7

--- Implementation/game.py
+++ Implementation/game.py
@@ -9,3 +9,32 @@ for i in range(N):
 sum=0
 num=0
 steps = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+
+# 반복문으로 감싸주기
+while True:
+    # set direction
+    D-=1
+    if D == -1:
+        D = 3
+    if map_list[A+(steps[D][0])][B+(steps[D][1])] == 0:
+        # check location
+        map_list[A+(steps[D][0])][B+(steps[D][1])] = 2
+        # move
+        A += steps[D][0]
+        B += steps[D][1]
+        # count
+        sum+=1
+        #initialize
+        num=0
+    else:
+        num+=1
+        if (num==4) and (map_list[A+(steps[D-2][0])][B+(steps[D-2][1])] == 1):
+            break
+        elif (num==4) and (map_list[A+(steps[D-2][0])][B+(steps[D-2][1])] != 1):
+            # move back
+            A += steps[D-2][0]
+            B += steps[D-2][1]
+            num=0
+
+print(sum)
