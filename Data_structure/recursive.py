6dd66f2712d3fd317df4f709713aae29d036e867
new-w <newww0120@gmail.com>
2021-01-14 15:56:10 +0900

    day9

--- Data_structure/recursive.py
+++ Data_structure/recursive.py
@@ -1,8 +1,6 @@
-def recursive_function(num):
-    print(num, ":", "번째 재귀함수 호출")
-    num+=1
-    if num == 101:
-        return
-    recursive_function(num)
+def factorial_recursive(n):
+    if n<=1:
+        return 1
+    return n*factorial_recursive(n-1)
 
-recursive_function(1)
+print(factorial_recursive(7))
