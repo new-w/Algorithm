bcab4efea729736005e2dc6b058c44d2a7436c55
new-w <newww0120@gmail.com>
2021-01-15 18:15:40 +0900

    modify

--- Data_structure/recursive.py
+++ Data_structure/recursive.py
@@ -1,13 +1,16 @@
+#iterative
 def factorial_iterative(n):
     result=1
-    i=1
-    for i in range(n+1):
-        result = result * i
+    for i in range(1, n+1):
+        result *=i
+    return result
 
+#recursive
 def factorial_recursive(n):
-    if n<=1:
+    if n==0:
         return 1
-    return n*factorial_recursive(n-1)
+    return factorial_recursive(n-1) * n
+
 
 print(factorial_iterative(7))
 print(factorial_recursive(7))
