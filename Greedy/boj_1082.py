2119252163aaffbcd658ed39a004bf1436e27176
new-w <newww0120@gmail.com>
2021-01-16 20:47:37 +0900

    base

--- Greedy/boj_1082.py
+++ Greedy/boj_1082.py
@@ -1,1 +1,10 @@
-#방 번호
+N = int(input())
+num_list = list(map(int, input().split()))
+money = int(input())
+
+result = 0
+
+# 1번째 : 자릿수 늘리기 (0으로 시작 안됨)
+# 2번째 : 큰 수 선택하기
+
+# -> 맨 첫 번째 자릿수에 뭐가 오는지에 따라 달라짐
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Algorithm
+
+### simulation문제 -> 문제 설명에 따라 순차적으로 구현
