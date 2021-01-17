92c8107ba89a592c5ab71f6093457caa925804e6
new-w <newww0120@gmail.com>
2021-01-17 17:58:54 +0900

    day10

--- Greedy/boj_1082.py
+++ Greedy/boj_1082.py
@@ -1,10 +1,18 @@
 N = int(input())
-num_list = list(map(int, input().split()))
+value_list = list(map(int, input().split()))
 money = int(input())
 
+money_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 result = 0
 
-# 1번째 : 자릿수 늘리기 (0으로 시작 안됨)
-# 2번째 : 큰 수 선택하기
+# 자릿수를 크게 만드는 방법 -> 최대한 많은 개수의 숫자를 구입 -> 최대 개수의 숫자로 이루어진 값들의 크기를 비교하여 최종 선택
 
-# -> 맨 첫 번째 자릿수에 뭐가 오는지에 따라 달라짐
+# 최대한 많은 개수의 숫자를 구입
+share_list = []
+for i in range(0, N):
+    share_list.append(money // value_list[N-1-i])
+
+for i in range(0, share_list[0]+1):
+    
+
+print(share_list)
