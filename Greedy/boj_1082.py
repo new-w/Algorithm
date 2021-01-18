dd36345cc5bb784978b0b5d86dc97c69ac092900
new-w <newww0120@gmail.com>
2021-01-18 15:37:25 +0900

    day11

--- Greedy/boj_1082.py
+++ Greedy/boj_1082.py
@@ -7,6 +7,7 @@ def convert(num):
 N = int(input())
 value_list = list(map(int, input().split()))
 money = int(input())
+cur_money = money
 
 money_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 final_result=0
@@ -14,15 +15,24 @@ final_result=0
 
 # 최대한 많은 개수의 숫자를 구입
 num_list = []
-if (money//value_list[1]!=0):
-    num_list.append(money_list[1])
-    base = (money % value_list[1]) // value_list[0]
-    if base!=0:
-        for k in range(base):
-            num_list.append(money_list[0])
+if N!=1:
+    if (money//value_list[1]!=0):
+        num_list.append(money_list[1])
+        cur_money-=value_list[1]
+        base = (money-value_list[1]) // value_list[0]
+        if base!=0:
+            for k in range(base):
+                num_list.append(money_list[0])
+                cur_money-=value_list[0]
+        
+        # 각 자리의 수를 최대 값으로 변경
+        
+
+    else:
+        final_result=0
 
 else:
     final_result=0
 
 final_result = convert(num_list)
-print(final_result)
+print(final_result, cur_money)
